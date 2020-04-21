a340e87f7e18eee759fbd0138a0d394a6ba942d8
root <root@ip-172-31-1-87.ap-southeast-2.compute.internal>
2020-04-21 10:13:06 +0000

    for bashing

---
+++ examplegit2.py
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+print'welcome to examplegit..HELLO divya!') 
+=======
+print'welcome to examplegit..HELLO DD2 modified in firstbranch') 
+>>>>>>> 756d75784cfb9515309df1830abef4ae86905291
